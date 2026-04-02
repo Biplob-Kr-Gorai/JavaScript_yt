defbd50a109b4b6ac49a1966725c2a10b5d3c6d2
Biplob Gorai <goraisachin768@gmail.com>
2026-04-02 15:37:28 +0000

    Everything about OOPS in JS

---
+++ 10_Classes_and_OOP/notes.md
@@ -0,0 +1,26 @@
+#Javascript and classes
+
+## OOP
+
+## Object
+-Collection of properties and methods
+-toLowerCase
+
+
+## Why we use OOP
+
+## Parts of OOP
+Object literal
+
+- Constructor Function
+- Prototypes
+- Classes
+- Instances (new, this)
+
+
+## 4 Pillars of OOP
+Abstraction
+Encapsulation
+Inheritance
+Polymorphism
+
---
+++ 10_Classes_and_OOP/oop.js
@@ -0,0 +1,41 @@
+// const { user } = require("react");
+
+const user = {
+    username : "Sachin",
+    loginCount : 8,
+    signedIn : true,
+
+    getUserDetails : function(){
+        // console.log("Got user details from database");
+        // console.log(`Username : ${this.username}`);
+        
+        console.log(this);
+        
+    }
+}
+
+console.log(user.username);
+// console.log(user.getUserDetails());
+console.log(this);
+
+
+function User(username, loginCount, isLoggedIn){
+    this.username = username;
+    this.loginCount = loginCount;
+    this.isLoggedIn = isLoggedIn;
+
+    return this
+}
+
+const userOne = new User("Sachin", 22, true)
+const userTwo = new User("Biplob", 21, false)
+
+console.log(userOne);
+console.log(userTwo);
+
+//Note :- first of all when we use " this " then an empty object is got created which we called as an instance
+// Everytime we use this an empty object/parentheses is cretated
+//Constructor function is called beacuse of " new " keyword 
+
+//javascript has prototyping behaviour
+
